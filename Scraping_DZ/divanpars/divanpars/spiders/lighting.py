00942a49ed32618e7e2aadb5bde6002f00319e86
blisscreationist <101796532+blisscreationist@users.noreply.github.com>
2024-07-09 23:11:11 +0300

    first code

---
+++ Scraping_DZ/divanpars/divanpars/spiders/lighting.py
@@ -0,0 +1,17 @@
+import scrapy
+
+class LightingSpider(scrapy.Spider):
+    name = "lighting"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        lightings = response.css('div._Ud0k')
+        for lighting in lightings:
+            yield {
+                'name': lighting.css('div.lsooF span::text').get(),
+                'price': lighting.css('div.pY3d2 span::text').get(),
+                'url': response.urljoin(lighting.css('a').attrib['href'])
+            }
+
+
